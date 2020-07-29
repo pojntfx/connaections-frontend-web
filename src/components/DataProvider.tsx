b9ea44402ac6af20c1d10d43b7f9b57beb98c301
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-07-29 14:36:20 +0200

    feat: Add basic subscriber implementation

---
+++ src/components/DataProvider.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { Connection } from "../proto/generated/src/proto/connections_pb";
+import { BrowserHeaders } from "browser-headers";
+import { ConnectionsClient } from "../proto/generated/src/proto/connections_pb_service";
+import { v4 } from "uuid";
+
+export interface IDataProviderProps {
+  token: string;
+  initialEndpoint: string;
+  children: (props: IDataProviderDataProps) => React.ReactElement;
+}
+
+export interface IDataProviderDataProps {
+  connections: Connection[];
+}
+
+export interface IIdentifiableConnection extends Connection {
+  id: string;
+}
+
+export const DataProvider: React.FC<IDataProviderProps> = ({
+  token,
+  initialEndpoint,
+  children,
+  ...otherProps
+}) => {
+  const [connections, setConnections] = React.useState<
+    IIdentifiableConnection[]
+  >([]);
+  const [endpoint, setEndpoint] = React.useState(initialEndpoint);
+
+  React.useEffect(() => {
+    const headers = new BrowserHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    const client = new ConnectionsClient(endpoint);
+
+    const stream = client.subscribe(new Connection(), headers);
+
+    stream.on("data", async (msg: IIdentifiableConnection) => {
+      msg.id = v4();
+
+      setConnections((oldConnections) => [...oldConnections, msg]);
+      setTimeout(() => {
+        console.log("Deleting", msg.id);
+        setConnections((oldConnections) =>
+          oldConnections.filter((connection) => connection.id != msg.id)
+        );
+      }, 100);
+    });
+  }, [endpoint]);
+
+  return connections.length != 0 ? (
+    children({ connections, ...otherProps })
+  ) : (
+    <div {...otherProps}>
+      <input
+        placeholder="Endpoint"
+        value={endpoint}
+        onChange={(e) => setEndpoint(e.target.value)}
+      />
+    </div>
+  );
+};
