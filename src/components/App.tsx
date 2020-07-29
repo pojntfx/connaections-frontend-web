768a430ac324938cb8b123d62b3260edb2cd55fd
Felicitas Pojtinger <felicitas@pojtinger.com>
2020-07-29 20:46:01 +0200

    feat: Add minimal CSS grid layout

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import { LoginProvider } from "./LoginProvider";
 import { DataProvider } from "./DataProvider";
 import { ConnectionsMap } from "./ConnectionsMap";
 import { ConnectionsList } from "./ConnectionsList";
+import styled, { createGlobalStyle } from "styled-components";
 
 export interface IAppProps {
   clientId: string;
@@ -19,12 +20,14 @@ export const App: React.FC<IAppProps> = ({
       <DataProvider endpoint={endpoint} token={token}>
         {({ connections, clearConnections }) => (
           <>
-            <header>
+            <GlobalStyle />
+
+            <Header>
               <h1>Connaections</h1>
               <button onClick={clearConnections}>Clear connections</button>
-            </header>
+            </Header>
 
-            <main>
+            <Main>
               <article>
                 <section id="map">
                   <h2>Connection Map</h2>
@@ -35,7 +38,7 @@ export const App: React.FC<IAppProps> = ({
                 </section>
               </article>
 
-              <aside>
+              <Aside>
                 <section id="count">
                   <h2>Connection Count</h2>
                   <span>{connections.length} individual connections</span>
@@ -45,11 +48,37 @@ export const App: React.FC<IAppProps> = ({
                   <h2>Connection List</h2>
                   <ConnectionsList connections={connections} />
                 </section>
-              </aside>
-            </main>
+              </Aside>
+            </Main>
           </>
         )}
       </DataProvider>
     )}
   </LoginProvider>
 );
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Main = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+
+  @media screen and (min-width: 992px) {
+    grid-template-columns: 2fr 1fr;
+  }
+`;
+
+const Aside = styled.aside`
+  min-width: 0;
+`;
